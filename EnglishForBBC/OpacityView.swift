33a6e6ad9f560bc57c241e1f5f8791a44db56c65
Duy Huan <duyhuan@Duys-MacBook-Pro.local>
2017-05-20 09:38:48 +0700

    EnglishForBBC

---
+++ EnglishForBBC/OpacityView.swift
@@ -0,0 +1,21 @@
+//
+//  OpacityView.swift
+//  EnglishForBBC
+//
+//  Created by Duy Huan on 5/19/17.
+//  Copyright © 2017 Duy Huan. All rights reserved.
+//
+
+import UIKit
+
+class OpacityView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ EnglishForBBC/SettingView.swift
@@ -0,0 +1,9 @@
+//
+//  SettingView.swift
+//  EnglishForBBC
+//
+//  Created by Duy Huan on 5/18/17.
+//  Copyright © 2017 Duy Huan. All rights reserved.
+//
+
+import Foundation
